530a6ee27d9d5a21c677a3c9b527e94f80bf59fd
Savila Saraiva <saraivasvfarias@gmail.com>
2019-04-05 06:27:03 -0300

    Page Home and Cardapio alt2

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ function scrollToTop() {
     offsetTop = offset.top;
     $('html, body').animate({ scrollTop: offsetTop }, 600, 'linear').animate({ scrollTop: 25 }, 200).animate({ scrollTop: 0 }, 150).animate({ scrollTop: 0 }, 50);
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
